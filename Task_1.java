03172952f1fbb03d52c8f0922d5d97004a60475c
Harshit0018 <hrautela497@gmail.com>
2025-01-19 23:31:16 +0530

    Display Address

---
+++ Task_1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Task_1{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        
+
+        System.out.print("Enter your address: ");
+        String address = scanner.nextLine();
+
+        System.out.println("Name: "+name);
+        System.out.println("Address: "+address);
+
+    }
+}
